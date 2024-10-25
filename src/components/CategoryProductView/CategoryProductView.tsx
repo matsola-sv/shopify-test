7e8dcecbaefd4aef751cc56c0677b5a6b05c66c5
matsola-sv <sergey.macola@gmail.com>
2024-10-25 12:10:42 +0200

    refactor: Remove dynamic import to optimize code.

--- src/components/CategoryProductView/CategoryProductView.tsx
+++ src/components/CategoryProductView/CategoryProductView.tsx
@@ -1,13 +1,9 @@
-import React, {FC, Suspense, useState} from 'react';
+import React, {FC, useState} from 'react';
 
 import { NullableString } from '../../models/common';
 
 import Categories from '../Categories/Categories';
-import Preloader from "../Common/Preloader/Preloader";
-
-// Dynamic loading components
-// Moved outside the function to avoid repeated React.lazy calls on every render of CategoryProductView.
-const ProductList = React.lazy(() => import('../ProductList/ProductList'));
+import ProductList from "../ProductList/ProductList";
 
 interface CategoryProductViewProps {
     productsPerPage?: number;
@@ -20,12 +16,10 @@ const CategoryProductView: FC<CategoryProductViewProps> = ({productsPerPage = 2}
         <div className="content-wrapper" data-testid="category-product-view">
             <Categories onSelectCategory={setActiveCategory} />
             {activeCategory && (
-                <Suspense fallback={<Preloader/>}>
-                    <ProductList
-                        categoryId={activeCategory}
-                        productsPerPage={productsPerPage}
-                    />
-                </Suspense>
+                <ProductList
+                    categoryId={activeCategory}
+                    productsPerPage={productsPerPage}
+                />
             )}
         </div>
     );
